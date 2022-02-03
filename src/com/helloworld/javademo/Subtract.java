9715a141079b54cb11c038bca4ae025d78a1df6c
17harshu <harshalanandas17@gmail.com>
2022-02-03 08:49:19 +0530

    Subtract of 2 variables

---
+++ src/com/helloworld/javademo/Subtract.java
@@ -0,0 +1,10 @@
+package com.helloworld.javademo;
+
+public class Subtract {
+	public static void main(String[] args) {
+		int a = 30;
+		int b = 20;
+		int sum = a-b;
+		System.out.println("Subtraction: " +sum);
+	}
+}
